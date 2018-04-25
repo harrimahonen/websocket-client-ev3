9afe9541cdce2fa7bc6df53aa5d50a4ce9e28922
Harri Mähönen <mahonen.h@gmail.com>
2018-04-25 21:29:05 +0300

    working websocket client implemented

--- src/App.js
+++ src/App.js
@@ -2,20 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const ws = new WebSocket('ws://localhost:8887')
+    ws.onopen = function() {
+     console.log("connection open...");
+   };
 class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+    }
+        this.handleClick = this.handleClick.bind(this)
+    } 
+    handleClick(cmd){
+       ws.send(JSON.stringify({
+            to: "sec-websocket-indetifier",
+            message: cmd }
+         )
+     )
+    }
+    componentDidMount(){
+    }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <div >
+        <p className="App" >
+            Click on the arrows to send web socket message to server
         </p>
+        <p onClick={(e) => this.handleClick("UP")} >{String.fromCharCode(8593)}</p>
+        <p onClick={(e) => this.handleClick("LEFT")} >{String.fromCharCode(8592)}</p>
+        <p onClick={(e) => this.handleClick("RIGHT")} >{String.fromCharCode(8594)}</p>
+        <p onClick={(e) => this.handleClick("DOWN")} >{String.fromCharCode(8595)}</p>
+    </div>
       </div>
     );
   }
 }
+
 
 export default App;
