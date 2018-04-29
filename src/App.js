389d7527d879792f95705912ce8e9a32204dce6c
Harri Mähönen <mahonen.h@gmail.com>
2018-04-29 18:41:46 +0300

    added area for color

--- src/App.js
+++ src/App.js
@@ -1,60 +1,73 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-    constructor(){
-        super()
-        this.state = {
-            color: false,
-            error: false
+  constructor () {
+    super()
+    this.state = {
+      color: false,
+      error: false
     }
-            
-    } 
-    handleClick(cmd){
-           this.ws.send(cmd)
-
+  }
+  handleClick (cmd) {
+    if (this.ws) {
+      this.ws.send(cmd)
     }
-    componentDidMount(){
-    this.ws = new WebSocket('ws://10.0.1.1:8887')
-        this.ws.onmessage = e => {
-            this.setState({ color: e.data })
-            console.log("success: " + e.data)
-            console.log(this.state)}
-    this.ws.onerror = e => this.setState({ error: 'WebSocket error' })
-    this.ws.onclose = e => !e.wasClean && this.setState({ error: `WebSocket error: ${e.code} ${e.reason}` })
-    //setInterval( () => {this.ws.send("STOP")}, 1000 )
-
+  }
+  componentDidMount () {
+    // COMMENT ONE this.ws line
+    // You need to know the EV3 IP to connect to it; wired connection default: 10.0.1.1
+    // this.ws = new WebSocket('ws://10.0.1.1:8887') // Use this when working with EV3 brick
+    this.ws = new WebSocket('ws:localhost:8887') // Use this when developing locally
+    this.ws.onopen = e => {
+      this.setState({
+        color: 'black'
+      })
+      console.log('WebSocket connection is ready! Current state: ')
+      console.log(this.state)
     }
-  render() {
+    this.ws.onerror = e => this.setState({
+      error: 'WebSocket error'
+    })
+    this.ws.onclose = e => !e.wasClean && this.setState({
+      error: `WebSocket connection closed: ${e.code} ${e.reason}`
+    })
+    this.ws.onmessage = e => {
+      this.setState({color: e.data})
+    }
+  }
+  render () {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <div style={{display: 'flex',
-            justifyContent: 'center',
-            marginTop: 25}}>
-            <div style={{
-        backgroundColor:this.state.color,
-        height:100,
-        width: 100
-            }}/>
+        <div><p>Current color:</p></div>
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          marginTop: 25
+        }}>
+          <div style={{
+            backgroundColor: this.state.color,
+            height: 200,
+            width: 200,
+            border: 'solid 2px black'
+          }}/>
         </div>
-        <p className="App" >
-            Click on the arrows to send web socket message to server
+        <p>
+            Control EV3 with arrows
         </p>
-        <button onMouseDown={(e) => this.handleClick("UP")} onMouseUp={(e) => this.handleClick("STOP")} >{String.fromCharCode(8593)}</button>
-        <button onMouseDown={(e) => this.handleClick("DOWN")} onMouseUp={(e) => this.handleClick("STOP")} >{String.fromCharCode(8595)}</button>
-	<button onMouseDown={(e) => this.handleClick("LEFT")} onMouseUp={(e) => this.handleClick("STOP")} >{String.fromCharCode(8592)}</button>
-	<button onMouseDown={(e) => this.handleClick("RIGHT")} onMouseUp={(e) => this.handleClick("STOP")} >{String.fromCharCode(8594)}</button>    
-	<button onMouseDown={(e) => this.handleClick("SABOTAGE")} >SABOTAGE</button>    
-</div>
-    );
+        <button onMouseDown={(e) => this.handleClick('UP')} onMouseUp={(e) => this.handleClick('STOP')} >{String.fromCharCode(8593)}</button>
+        <button onMouseDown={(e) => this.handleClick('DOWN')} onMouseUp={(e) => this.handleClick('STOP')} >{String.fromCharCode(8595)}</button>
+        <button onMouseDown={(e) => this.handleClick('LEFT')} onMouseUp={(e) => this.handleClick('STOP')} >{String.fromCharCode(8592)}</button>
+        <button onMouseDown={(e) => this.handleClick('RIGHT')} onMouseUp={(e) => this.handleClick('STOP')} >{String.fromCharCode(8594)}</button>
+        <button onMouseDown={(e) => this.handleClick('SABOTAGE')} >SABOTAGE</button>
+      </div>
+    )
   }
 }
 
-
-export default App;
+export default App
--- src/Server.js
+++ src/Server.js
@@ -1,39 +1,34 @@
 // server.js
-const WebSocket = require('ws');
-var Server = require('ws').Server;
-var port = process.env.PORT || 9030;
-var ws = new Server({port: port});
+const WebSocket = require('ws')
+var Server = require('ws').Server
+var port = process.env.PORT || 9030
+var ws = new Server({port: port})
 
-ws.on('connection', function(w){
-  
-  w.on('message', function(msg){
-    console.log('message from client');
-  });
-  
-  w.on('close', function() {
-    console.log('closing connection');
-  });
+ws.on('connection', function (w) {
+  w.on('message', function (msg) {
+    console.log('message from client')
+  })
 
-});
+  w.on('close', function () {
+    console.log('closing connection')
+  })
+})
 
-
-var connection = new WebSocket('ws://localhost:8887');
+var connection = new WebSocket('ws://localhost:8887')
 
 connection.onopen = function () {
-    var data = {
-    to: "sec-websocket-identifier",
+  var data = {
+    to: 'sec-websocket-identifier',
     message: 'hello from client 1'
-};
-connection.send(JSON.stringify(data));
-};
+  }
+  connection.send(JSON.stringify(data))
+}
 
 // Log errors
 connection.onerror = function (error) {
-  console.error('WebSocket Error ' + error);
-};
+  console.error('WebSocket Error ' + error)
+}
 
 // Log messages from the server
 connection.onmessage = function (e) {
-
-};
-
+}
